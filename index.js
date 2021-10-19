23768d82962323ef5e2defb821e87159908bb018
Robert <bates.to.infinity@gmail.com>
2021-10-19 15:04:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-var cats = ["Milo", "Otis", "Garfield"]
+const cats = ["Milo", "Otis", "Garfield"]
 
+function destructivelyAppendCat() {
     cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat() {
+   const broom = [...cats, "Broom"]
+}
+
+function prependCat() {
+
+}
